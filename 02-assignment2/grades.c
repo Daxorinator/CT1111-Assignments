23681386bdedae0c9f43680183e0c1ab4c6f892f
CrashOverride <daxorinator@gmail.com>
2022-01-28 14:28:35 +0000

    Add comments to Assignment 2

--- 02-assignment2/grades.c
+++ 02-assignment2/grades.c
@@ -2,22 +2,28 @@
 #include <math.h>
 
 void main() {
+    // Define & Initialise scores & weights
     float lab = 0.0f, exam = 0.0f, assignment = 0.0f;
     float lab_weight = 0.5f, exam_weight = 0.25f, assignment_weight = 0.25f;
 
+    // Get score for lab
     printf_s("Enter score for lab:");
     scanf_s("%f", &lab);
 
+    // Get score for exam
     printf_s("Enter score for exam:");
     scanf_s("%f", &exam);
 
+    // Get score for assignment
     printf_s("Enter score for the assignment:");
     scanf_s("%f", &assignment);
 
+    // Calculate final score by multiplying scores with weights, adding them and rounding down.
     float final_score = floorf(
         (lab * lab_weight) + (exam * exam_weight) + (assignment * assignment_weight)
     );
 
+    // Match final scores to letter grades by sifting upward from 40%
     if (final_score < 40.0f) {
         printf_s("Letter Grade: E");
     } else if (final_score < 50.0f) {
